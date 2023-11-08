f5ce88078e9677b148e069d52cd7f547b45cbcf5
mshar <email@mail.com>
2023-11-08 16:30:56 +0300

    adding Projects page

---
+++ assets/Contact-me-80790943.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-4926d740.js";const o=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"Contact Me"}),e.jsx("p",{children:"Feel free to reach out to me at example@example.com"})]});export{o as default};
